89731e1a8e4b4e5f4b750f999f406f0c1aa0e94f
Srisha-24 <srisharamesh2004@gmail.com>
2025-07-02 21:34:32 +0530

    Delete JAVA directory

--- JAVA/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(int id) {
-        super("Student not found with ID: " + id);
-    }
-}
