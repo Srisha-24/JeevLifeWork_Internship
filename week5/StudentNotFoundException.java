781be2070c99f276f79afc97e916e15e9433378d
Srisha-24 <srisharamesh2004@gmail.com>
2025-07-07 20:56:54 +0530

    Delete week5 directory

--- week5/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentcrud.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
